9afb9232f71757a51a1894a537c1bb7829641f53
OLC <ritsukage00@gmail.com>
2025-01-21 10:41:42 +0800

    fix embed path

--- src/RitsukageGif/Class/EmbeddedResourcesHelper.cs
+++ src/RitsukageGif/Class/EmbeddedResourcesHelper.cs
@@ -11,12 +11,15 @@ namespace RitsukageGif.Class
             if (uri.Scheme != "embedded")
                 throw new NotSupportedException("Scheme not supported");
 
-            var assembly = Assembly.GetExecutingAssembly();
+            assembly ??= Assembly.GetExecutingAssembly();
             var defaultNamespace = assembly.GetName().Name;
-            var resourcePath = $"{defaultNamespace}{uri.AbsolutePath.Replace('/', '.').Replace('\\', '.')}";
-            var stream = assembly.GetManifestResourceStream(resourcePath);
+            var resourcePath = uri.Host + uri.AbsolutePath;
+            if (!resourcePath.StartsWith('/'))
+                resourcePath = '/' + resourcePath;
+            resourcePath = $"{defaultNamespace}{resourcePath.Replace('\\', '.').Replace('/', '.')}";
+            var stream = assembly.GetManifestResourceStream(resourcePath) ?? throw new InvalidOperationException($"Resource not found: {resourcePath}");
             var memoryStream = new MemoryStream();
-            stream?.CopyTo(memoryStream);
+            stream.CopyTo(memoryStream);
             memoryStream.Seek(0, SeekOrigin.Begin);
             return memoryStream;
         }
